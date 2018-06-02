6ab8a636b20b038b3abcc32986cde3eaa9fa956d
priya <priya@LAPTOP-QP1B3T83>
2018-06-01 20:36:30 -0600

    Added areas package Added python .project files

---
+++ test/TestAreas.py
@@ -0,0 +1,12 @@
+import Volumes.areas
+'''
+Created on Jun 1, 2018
+
+@author: priya
+'''
+from Volumes.areas import area_circle
+
+print("Area of circle = "+str(area_circle(10)))
+
+if __name__ == '__main__':
+    pass
