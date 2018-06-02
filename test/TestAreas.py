e97eac6c5a45bbdbc141ea4ce566d16ad1e99684
priya <priya@LAPTOP-QP1B3T83>
2018-06-01 20:56:25 -0600

    Added square

--- test/TestAreas.py
+++ test/TestAreas.py
@@ -1,6 +1,8 @@
 import areas.areas
 
+
 print("Area of circle = "+str( areas.areas.area_circle(10)))
+print("Area of square = "+str(areas.areas.area_square(10)))
 
 if __name__ == '__main__':
     pass
