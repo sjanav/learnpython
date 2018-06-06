247b900ee1693e192cfb59e4b2165622c1d03201
priya <priya@LAPTOP-QP1B3T83>
2018-06-05 18:26:05 -0600

    I reversed the list

--- test/openingFiles.py
+++ test/openingFiles.py
@@ -4,3 +4,8 @@ myfile = open("C:\\Users\\priya\\tmp\\weekdays1.txt","r")
 lines = myfile.readlines()
 myfile.close
 print(lines)
+
+
+#Now I am going to use a method to reverse a list.
+lines.reverse()
+print(lines)
