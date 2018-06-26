d038f55f94eedc0085c6e1dae58dc88843f689a8
saijanav <saijanav@LAPTOP-59HKQASR>
2018-06-26 11:11:16 -0600

    checkimg in ip adress test

---
+++ test/TestIpAddress.py
@@ -0,0 +1,22 @@
+import socket
+while("true"):
+    web = input("Enter a website name:")
+    ip = socket.gethostbyname(web)
+    print(ip)
+    
+    if not ip:
+        print("You entered an invalid website")
+    else:
+        print("The IP address is".format(ip))  
+    
+    cont = input("Do you want to continue? Press Y to continue")
+    if  cont != 'Y':
+            break 
+    
+    
+
+
+
+
+   
+    
